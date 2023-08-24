7f73851a2f1e11f1b62205deaddf926a502af01e
Enyone Christian Achobe <41975164+AEnyChris@users.noreply.github.com>
2023-08-24 04:54:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
-name = "Enyone Achobe"
-os = "Windows 10 Pro"
+name = "Enyone Christian Achobe"
+os = "Windows 10 Professional"
 print(f"Hi! My name is {name} and I developed this app in {os}, but you can run it everywhere :)")
